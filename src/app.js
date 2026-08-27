7a377ad1ceca887912e51b2e27f14664d8dd6429
warren-west <teacher.warren.english@gmail.com>
2026-08-27 11:25:01 +0200

    chore: Added CORS to the API.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require('cors')
+
 const trollsRouter = require("./routes/trolls");
 
 const app = express();
 
+app.use(cors()) // allow all origins
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Troll API!" });
