6c6a390941b302b3358847502593fbae15a18b2c
warren-west <teacher.warren.english@gmail.com>
2026-08-28 09:30:47 +0200

    fix: Added the hosted FE origin to CORS rules.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const trollsRouter = require("./routes/trolls");
 
 const app = express();
 
-app.use(cors()) // allow all origins
+app.use(cors({ origin: 'https://troll-fe-demo.onrender.com' })) // allow only one origin
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Troll API!" });
